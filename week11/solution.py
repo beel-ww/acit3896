3d3bf32d54bf059353d488f9b0f67e5ccad6084c
Bill <bliao6@proton.me>
2025-11-14 20:53:25 -0800

    week 11 lab jeremy edition

--- week11/solution.py
+++ week11/solution.py
@@ -6,16 +6,19 @@ def cheat_sort(seq):
 def cheat_merge(left, right):
     return sorted(left + right)
 
-def mergesort(seq):
+def merge_sort(seq):
     n = len(seq)
     if n <= 1:
         return seq
     
     else:
         middle = n // 2
-        left = mergesort(seq[:middle])
-        right = mergesort(seq[middle:])
-        return merge(left, right)
+        left = merge_sort(seq[:middle])
+        right = merge_sort(seq[middle:])
+        vals = merge(left, right)
+        for i in range(len(vals)):
+            seq[i] = vals[i]
+        return vals
 
 def merge(left: List[int], right: List[int]):
     i, j = 0, 0
@@ -44,4 +47,4 @@ left = [1,3,6,8,9]
 right = [2,4,5,7,10]
 
 print(merge(left, right))
-print(mergesort([33,1,66,7,5,33]))
+print(merge_sort([33,1,66,7,5,33]))
